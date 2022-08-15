0f2de45f0c3343c04f6fbff452f752e28461d9ac
u_m1gbg <ODanilyuk@alfabank.ru>
2022-08-15 11:29:30 +0300

    correct project structure, add dependencies

---
+++ src/test/java/com/example/kidsapp/KidsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kidsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
