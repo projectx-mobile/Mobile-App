b526bc65dcfed6b592a3c9ad3cb0aa7dc7440a72
Volga <84406564+VolgaDaniluk@users.noreply.github.com>
2022-08-16 16:18:48 +0300

    correct project structure, add dependencies (#2)

---
+++ src/main/java/com/example/kidsapp/KidsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.kidsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsAppApplication.class, args);
+	}
+
+}
