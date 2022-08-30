1da353d2f1e639195457bc0b4a0e4968434f08ae
Daniil Goncharov <brovar002@gmail.com>
2022-08-30 21:53:10 +0300

    feat(auth): add simple google auth

---
+++ authorization/src/main/java/com/jungeeks/Application.java
@@ -0,0 +1,14 @@
+package com.jungeeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.jungeeks")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+
+}
