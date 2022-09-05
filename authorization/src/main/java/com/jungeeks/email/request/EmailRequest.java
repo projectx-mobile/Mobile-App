fdee3fec288c86f3e92e8aab37398da76c99a4ec
nevels <neevels@mail.ru>
2022-09-05 12:22:27 +0300

    added Email Request

---
+++ authorization/src/main/java/com/jungeeks/email/request/EmailRequest.java
@@ -0,0 +1,15 @@
+package com.jungeeks.email.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@AllArgsConstructor
+public class EmailRequest {
+    @Email
+    @NotBlank
+    private String email;
+}
