4d8fe8d4a50c8b63e39bd509744a8e9c90581c0a
Daniil Goncharov <brovar002@gmail.com>
2022-09-06 17:05:01 +0300

    refactor(auth): add sub param

--- authorization/src/main/java/com/jungeeks/email/entity/User.java
+++ authorization/src/main/java/com/jungeeks/email/entity/User.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 public class User {
     @Id
     private Long id;
+    private Long sub;
     private String email;
     private String name;
     private String password;
