b9601a8e817ddc4cada445564231950dac16fa61
dmitry <anle751@gmail.com>
2022-10-06 23:32:08 +0300

    * init

---
+++ authorization/src/main/java/com/jungeeks/auth/models/CookieProperties.java
@@ -0,0 +1,12 @@
+package com.jungeeks.auth.models;
+
+import lombok.Data;
+
+@Data
+public class CookieProperties {
+    String domain;
+    String path;
+    boolean httpOnly;
+    boolean secure;
+    int maxAgeInMinutes;
+}
