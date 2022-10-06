ce111cb432c3ef1a998600971ecda6c9b80d06ca
nevels <neevels@mail.ru>
2022-10-06 18:23:34 +0300

    update security structure

--- authorization/src/main/java/com/jungeeks/security_config/PasswordEncoder.java
+++ authorization/src/main/java/com/jungeeks/security_config/PasswordEncoder.java
@@ -6,8 +6,5 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class PasswordEncoder {
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+
 }
