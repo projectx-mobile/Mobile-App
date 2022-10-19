967eb280d0d2491ebdee0a42694299e846742977
Dzmitry Lebedzenka <93610457+anle751@users.noreply.github.com>
2022-10-19 21:26:22 +0300

    * clean

--- application/src/main/java/com/jungeeks/repository/SocialCredentialsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jungeeks.repository;
-
-import com.jungeeks.entitiy.SocialCredentials;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SocialCredentialsRepository extends JpaRepository<SocialCredentials, Long> {
-}
--- application/src/main/java/com/jungeeks/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jungeeks.repository;
-
-import com.jungeeks.entitiy.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
