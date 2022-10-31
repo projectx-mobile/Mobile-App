c696996fb35cd98fe87fa7dc80ca124007672370
TorusTredent <torus.tredent@gmail.com>
2022-10-31 12:07:21 +0300

    fix nameing and delete annotation construct

--- accounts/src/main/java/com/jungeeks/dto/ParentHomeDto.java
+++ accounts/src/main/java/com/jungeeks/dto/ParentHomeDto.java
@@ -1,15 +1,11 @@
 package com.jungeeks.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class ParentHomeDto {
 
